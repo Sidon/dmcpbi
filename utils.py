81f0f2ea7e1ad77f1795231fbddde6d0f50c78e9
Sidon Duarte <sidoncd@gmail.com>
2018-10-23 11:44:41 -0300

    First commit

---
+++ utils.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Set some pandas options
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_rows', 10)
+
+
+class Dataset:
+    def __init__(self):
+        cols=["age","job","marital","education","default","balance","housing","loan","contact","day","month",
+            "duration","campaign","pdays","previous","poutcome","y"]
+        # Load from CSV
+        self.data = pd.DataFrame(pd.read_csv('data/bank-full.csv', skip_blank_lines=True,
+                                    usecols=cols, sep=';'))
+
+    def get_data(self, data_slice):
+        return self.data[data_slice]    
+
+    
